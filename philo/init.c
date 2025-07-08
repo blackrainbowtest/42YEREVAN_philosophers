f32389bca5b5120501b9cc74dcbf90ddf9dfc80c
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-07-08 22:54:23 +0400

    add

--- philo/init.c
+++ philo/init.c
@@ -2,6 +2,16 @@
 
 void	data_init(t_table *table)
 {
+	int	i;
+
+	i = -1;
 	table->end_simulation = false;
-	table->philos = safe_malloc(table->philo_nbr);
+	table->philos = safe_malloc(sizeof(t_philo) * table->philo_nbr);
+	table->forks = safe_malloc(sizeof(t_fork) * table->philo_nbr);
+	while (++i < table->philo_nbr)
+	{
+		safe_mutex_handle(&table->forks[i].fork, INIT);
+		table->forks[i].fork_id = i;
+	}
+	
 }
