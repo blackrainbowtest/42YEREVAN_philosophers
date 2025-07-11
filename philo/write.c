505ad706d95f744d1e105a7e9c35feffa46ab58e
¨blackrainbowtest¨ <araqelyan.aram.96@gmail.com>
2025-07-11 19:29:55 +0400

    add

--- philo/write.c
+++ philo/write.c
@@ -1,12 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   write.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 19:26:55 by aramarak          #+#    #+#             */
+/*   Updated: 2025/07/11 19:29:52 by aramarak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
 /**
  * [time_ms] [philo_id] [action]
  * 
  * thread safe
+ * timestamp_in_ms [philosopher_number] action nl
  */
 
-void   write_status(t_status status, t_philo *philo, bool debug)
+void	write_status(t_status status, t_philo *philo, bool debug)
 {
 	long	elapsed;
 
@@ -16,9 +29,8 @@ void   write_status(t_status status, t_philo *philo, bool debug)
 		write_status_debug(status, philo, elapsed);
 	else
 	{
-		// timestamp_in_ms philosopher_number action
-		if ((TAKE_FIRST_FORK == status || TAKE_SECOND_FORK == status) &&
-			!simulation_finish(philo->table))
+		if ((TAKE_FIRST_FORK == status || TAKE_SECOND_FORK == status)
+			&& !simulation_finish(philo->table))
 			printf(W"%-6ld"RST"\t[%d]\ttake a fork\n", elapsed, philo->id);
 		else if (EATING == status && !simulation_finish(philo->table))
 			printf(W"%-6ld"C"\t[%d]\teating\n"RST, elapsed, philo->id);
