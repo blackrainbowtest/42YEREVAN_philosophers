db70218538906880ca7227252be95a84849b1221
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-07-12 15:22:48 +0400

    add

--- philo/monitor.c
+++ philo/monitor.c
@@ -1,12 +1,47 @@
 #include "philo.h"
 
+/**
+ * t_die
+ * last_meal____________________last_meal
+ * maybe philo full
+ */
+static bool	philo_died(t_philo *philo)
+{
+	long	elapsed;
+	long	t_to_die;
+
+	if (get_bool(&philo->philo_mutex, &philo->full))
+		return (false);
+	elapsed = gettime(MILISECOND) - get_long(&philo->philo_mutex, philo->last_meal_time);
+	t_to_die = philo->table->time_to_die;
+
+	if (elapsed > t_to_die)
+		return (true);
+	return (false);
+}
+
 void	*monitor_dinner(void *data)
 {
+	int		i;
 	t_table *table;
 
+	i = -1;
 	table = (t_table *)data;
-	while (!all_threads_running())
+	// make sure all philo running
+	// spinlock till all thread run
+	while (!all_threads_running(&table->table_mutex, table->threads_running_number,
+			table->philo_nbr))
+		;
+	while (!simulation_finish(table))
 	{
-		
+		i = -1;
+		while (++i < table->philo_nbr && !simulation_finish(table))
+		{
+			if (philo_died(table->philos + i)) //TODO
+			{
+				set_bool(&table->table_mutex, &table->end_simulation, true);
+				write_status(DIED, table->philos + i, DEBUG_MODE);
+			}
+		}
 	}
 }
