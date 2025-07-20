34be5b83579853d83a61bcfb7665880f5a0e0be5
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-07-20 23:47:03 +0400

    update: types_bonus.h

--- bon2/includes/types_bonus.h
+++ bon2/includes/types_bonus.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 20:18:39 by root              #+#    #+#             */
-/*   Updated: 2025/07/20 22:54:18 by root             ###   ########.fr       */
+/*   Updated: 2025/07/20 23:46:51 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,54 @@
 
 # include <semaphore.h>
 
+struct s_table;
+
 // semaphores
 typedef struct s_sem
 {
-	
+	//"/die_sem"
+	sem_t	*die_sem;
+	//"/fork_sem"
+	sem_t	*form_sem;
+	//"/meal_sem"
+	sem_t	*meal_sem;
+	//"/write_sem"
+	sem_t	*write_sem;
 }	t_sem;
 
 // time counters
 typedef struct s_time
 {
-	
+	//time_to_die
+	long	time_to_die;
+	//time_to_eat
+	long	time_to_sleep;
+	//time_to_sleep;
+	long	time_to_sleep;
+	//time_last_meal;
+	long	time_last_meal;
+	//time_to_start;
+	long	time_to_start;
 }	t_time;
 
 // philo
 typedef struct s_philo
 {
-	/* data */
+	long			id;
+	long			meals_eaten;
+	t_time			*time;
+	t_sem			*sem;
+	struct s_table	*table;
 }	t_philo;
 
 // table
 typedef struct s_table
 {
-	
+	long			philo_count;
+	long			meals_limit;
+	t_philo			**philo;
+	t_time			*time;
+	t_sem			*sem;
 }	t_table;
+
 #endif	/* TYPES_BONUS_H */
