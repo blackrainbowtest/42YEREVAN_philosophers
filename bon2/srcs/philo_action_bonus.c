222bf5a3d65395d7e45c174b7a9700c90890ea6c
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2025-07-22 17:30:47 +0400

    update: philo_sleep

--- bon2/srcs/philo_action_bonus.c
+++ bon2/srcs/philo_action_bonus.c
@@ -3,15 +3,18 @@
 void	take_forks(t_philo *philo)
 {
 	safe_sem_handle(&philo->table->sem->forks, WAIT);
-	write_status(TAKE_FORK, philo, false);
+	write_status(TAKE_FORK, philo, DEBUG_MODE);
 	safe_sem_handle(&philo->table->sem->forks, WAIT);
-	write_status(TAKE_FORK, philo, false);
+	write_status(TAKE_FORK, philo, DEBUG_MODE);
 }
 
 void	philo_eat(t_philo *philo)
 {
+	long	time_to_eat;
+
+	time_to_eat = philo->table->time->time_to_eat;
 	philo->time_last_meal = get_time(philo->table, MILLISECOND);
-	write_status(EATING, philo, false);
+	write_status(EATING, philo, DEBUG_MODE);
 	philo->meals_counter++;
 	if (philo->table->max_meals > 0
 		&& philo->meals_counter >= philo->table->max_meals)
@@ -19,11 +22,22 @@ void	philo_eat(t_philo *philo)
 		philo->full = true;
 		safe_sem_handle(&philo->table->sem->meal_sem, POST);
 	}
-	precise_usleep(philo->table->time->time_to_eat * 1e3, philo->table);
+	precise_usleep(time_to_eat * 1e3, philo->table);
 }
 
 void	drop_forks(t_philo *philo)
 {
 	safe_sem_handle(&philo->table->sem->forks, POST);
 	safe_sem_handle(&philo->table->sem->forks, POST);
+}
+
+void	philo_sleep(t_philo *philo)
+{
+	long	time_to_sleep;
+
+	time_to_sleep = philo->table->time->time_to_sleep;
+	if (simulation_finish(philo->table))
+		return ;
+	write_status(SLEEPING, philo, DEBUG_MODE);
+	precise_usleep(time_to_sleep * 1e3, philo->table);
 }
